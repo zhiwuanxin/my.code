63c7d742c65477bb462505ff567d3db8c9520605
zhiwuan <1955468560@qq.com>
2026-04-02 20:09:15 +0800

    test

---
+++ largest_subsequent/greed_binary.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+const int N = 1e5;
+int n, arr[N], dp[N], len;
+
+int main()
+{
+    cin >> n;
+    memset(dp, 0x3f, sizeof(dp));
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(i == 1 || arr[i] > dp[len]) dp[++len] = arr[i];
+        else
+        {
+            int l = 1, r = len;
+            while(l < r)
+            {
+                int mid = (l + r) / 2;
+                if(dp[mid] > arr[i]) r = mid;
+                else l = mid + 1;
+            }
+
+        }
+    }
+    cout << len;
+    return 0;
+}
